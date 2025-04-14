6daf7e10553cbcbc47dff1343b9012c5d3670e78
Somesthetic <forest-denton@ku.edu>
2025-04-14 17:24:46 -0500

    started on calculator.hpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,12 @@
 #include "calculator.hpp"
+
+bool is_valid_conversion(const std::string the_string){
+  bool has_decimal = false;
+  for (int index = 0; index < size(the_string); index++){
+    if (the_string[index] == '.'){
+      has_decimal = true;  // if the val is a decimal change the variable
+    } else if (the_string[index] == '+' or the_string[index] == '-'){
+    }  // no issues if value is + or -
+
+  }
+}
--- calculator.hpp
+++ calculator.hpp
@@ -6,5 +6,7 @@
 // this function should expect that `expression` has already been
 // validated and thus throw no exceptions
 double parse_number(const std::string &expression);
+bool is_valid_conversion(const std::string the_string);  // function for determining if the string is valid for conversion
+
 
 #endif // __CALCULATOR_HPP
